9388b0d59755aabcc85acffed7a111f3dd986de2
bngure <brianwahinyangure@gmail.com>
2022-01-31 15:08:16 +0300

    Imported pydantic Basemodel to define schema in posts, Imported Optional from typing library

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,3 +18,16 @@ async def get_posts():
             {2: "Post Two"}
         ]
     }
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+@app.post("/createposts")
+async def create_posts(post: Post):
+    print(post)
+    return {"post": f"title: {post.title}, content: {post.content}, published: {post.published}, rating: {post.rating}"}
