18ce1304b17f4c8395ccba1cb51b98309154f716
bngure <brianwahinyangure@gmail.com>
2022-01-31 14:19:09 +0300

    Added path-operation /posts in main

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {
+        "data": [
+            {1: "Post One"},
+            {2: "Post Two"}
+        ]
+    }
